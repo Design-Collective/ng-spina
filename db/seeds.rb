dc9f2e48165ce07048ac5248a5a9463fdcd7fc6b
sugarskull <rodrigo@designcollective.io>
2016-03-14 20:14:34 -0700

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Page.create([
+  { title: 'Home' },
+  { title: 'About Us' },
+  { title: 'Clients' },
+  { title: 'Process' },
+  { title: 'Portfolio' },
+  { title: 'Contact' },
+])
