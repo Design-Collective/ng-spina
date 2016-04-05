d7eb4e961e25c419cc655ad12a3b71b94b39fa90
sugarskull <rodrigo@designcollective.io>
2016-04-04 21:31:53 -0700

    add implicit render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::API
   #https://github.com/sferik/rails_admin/issues/1996
   include ::ActionController::Flash
   include ::ActionView::Layouts
+  include ::ActionController::ImplicitRender
   include ::ActionController::Helpers
   include ::ActionController::MimeResponds
   include ::ActionController::RequestForgeryProtection
