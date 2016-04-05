7ebed0ccc19c0e1b94ec4b9126a5f22ef3f485b3
sugarskull <rodrigo@designcollective.io>
2016-04-04 19:49:23 -0700

    layout :get_layout problems

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  #https://github.com/sferik/rails_admin/issues/1996
+  include ::ActionController::Flash
+  include ::ActionView::Layouts
+  include ::ActionController::Helpers
+  include ::ActionController::MimeResponds
+  include ::ActionController::RequestForgeryProtection
 end
