7cd709a8cf272edc7f58b57c99f15b0153b48888
sugarskull <rodrigo@designcollective.io>
2016-04-09 17:45:57 -0700

    fixes authorize_resource

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::API
-  #https://github.com/sferik/rails_admin/issues/1996
+  # https://github.com/sferik/rails_admin/issues/1996
   include ::ActionController::Flash
   include ::ActionView::Layouts
   include ::ActionController::ImplicitRender
   include ::ActionController::Helpers
   include ::ActionController::MimeResponds
   include ::ActionController::RequestForgeryProtection
+  # CanCan https://github.com/ryanb/cancan/wiki/authorizing-controller-actions
+  include ::ActionController::Base
 end
