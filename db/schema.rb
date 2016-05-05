27c1c7667dce5c34128dab7a0cee35ebfb1c6f72
Andrii Riepin <a.ryepin@svitla.com>
2016-05-05 16:30:20 +0300

    Add icon model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412134235) do
+ActiveRecord::Schema.define(version: 20160503150745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,12 @@ ActiveRecord::Schema.define(version: 20160412134235) do
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "spina_icons", force: :cascade do |t|
+    t.string   "icon_class"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "spina_inquiries", force: :cascade do |t|
